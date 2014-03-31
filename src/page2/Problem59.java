0d0f66c733d0d6f149111ecd800e5626459fcf29
Nick Barnes <nickbarnes89@gmail.com>
2014-03-31 18:17:22 -0500

    Problem 59

--- src/page2/Problem59.java
+++ src/page2/Problem59.java
@@ -1,15 +1,75 @@
 package page2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Problem59 {
 
 	public static void main(String[] args) {
 		
-		Scanner reader = new Scanner("resources/cipher1.txt");
-		while (reader.hasNextInt()) {
-			System.out.println(reader.nextInt());
+		int passwordLength = 3;
+		List<Character> password = new ArrayList<Character>(passwordLength);
+		
+		Scanner reader = null;
+		try {
+			reader = new Scanner(new File("resources/cipher1.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		System.out.println((char)('A' ^ '*'));
+		
+		List<Integer> cipherChars = new ArrayList<Integer>();
+		reader.useDelimiter(",");
+		while (reader.hasNextInt()) {
+			cipherChars.add(reader.nextInt());
+		}
+		
+		Map<Integer,Integer> letterCounts;
+		for (int i = 0; i < passwordLength; i++) {
+			int index = i;
+			letterCounts = new TreeMap<Integer,Integer>();
+			
+			while (index < cipherChars.size()) {
+				Integer nextChar = cipherChars.get(index);
+				if (letterCounts.containsKey(nextChar)) {
+					letterCounts.put(nextChar, letterCounts.get(nextChar)+1);
+				}
+				else {
+					letterCounts.put(nextChar, 1);
+				}
+				index += passwordLength;
+			}
+			
+			int max = Collections.max(new ArrayList<Integer>(letterCounts.values()));
+			for (Integer key : letterCounts.keySet()) {
+				if (letterCounts.get(key) == max) {
+					password.add((char) (' ' ^ key));
+					break;
+				}
+			}
+		}
+		
+		for (char c : password) {
+			System.out.print(c);
+		}
+		System.out.println();
+		
+		int i = 0;
+		int sum = 0;
+		for (int c : cipherChars) {
+			char decrypt = (char)((char)c ^ password.get(i%passwordLength));
+			System.out.print(decrypt);
+			sum += decrypt;
+			i++;
+		}
+		System.out.println();
+		System.out.println("Sum of ascii is " + sum);
 	}
 }
