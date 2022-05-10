bb2b94caa5dfc79f5871d406ef1f20005b2f9401
Nicholas Barnes <nickbaxter883@gmail.com>
2022-05-09 23:53:34 -0500

    Update p106.py
    A much nicer solution.

--- src/page3/p106.py
+++ src/page3/p106.py
@@ -1,25 +1,19 @@
-# Surely there is a less gross way to iterate this.
-# Need to find the number of ways two subsets of size k
-# such that each variable in one subset is greater than the
-# corresponding variable in the other subset.
-# Ex A + B + E < C + D + F because A < C, B < D, E < F or seen as a binary array
-# All 1's in 110010 are to the left of 1's in 001101
-
 from math import comb
+from itertools import combinations
 
 
-def definitively_unequal_pairs(n, k, lead):
-    if n < 0:
-        return 0
-    if k == 0:
-        return 1
+def definitively_unequal_pairs(n, k):
+    num_unequal_pairs = 0
+    for combo in combinations(list(range(n)), 2*k):
+        for sub_combo in combinations(combo, k):
+            other_sub_combo = [item for item in combo if item not in sub_combo]
+            for i in range(len(sub_combo)):
+                if sub_combo[i] > other_sub_combo[i]:
+                    break
+            else:
+                num_unequal_pairs += 1
 
-    num_pairs = 0
-    for i in range(n - 2*k + 1):
-        for j in range(max(i+1, lead), n):
-            num_pairs += definitively_unequal_pairs(n-2-i, k-1, j-1-i)
-
-    return num_pairs
+    return num_unequal_pairs
 
 
 N = 12
@@ -28,7 +22,7 @@ unequal_without_testing = 0
 
 for size in range(2, N//2+1):
     same_sized_subset_pairs += comb(N, size) * comb(N - size, size) // 2
-    unequal_without_testing += definitively_unequal_pairs(N, size, 0)
+    unequal_without_testing += definitively_unequal_pairs(N, size)
 
 print(same_sized_subset_pairs, unequal_without_testing)
 print(same_sized_subset_pairs - unequal_without_testing)
