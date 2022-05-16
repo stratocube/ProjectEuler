4d9b34d518559c230d658d45138e2291516bf127
Nicholas Barnes <nickbaxter883@gmail.com>
2022-05-16 13:30:01 -0500

    Update p132.py
    This solution works. Takes half a minute.

--- src/p126-150/p132.py
+++ src/p126-150/p132.py
@@ -1,65 +1,75 @@
-from sympy import primerange, factorint
+from sympy import primerange, isprime
+from timeit import timeit
 
+factor_list = set()
 
-def spaced_ones(count, spacing):
-    num = 0
-    for i in range(count):
-        num += 10 ** (i*spacing)
-    return num
-
-
-def alternating_ones(odd, spacing):
-    num1 = spaced_ones((odd+1)//2, 2*spacing)
-    num2 = spaced_ones((odd-1)//2, 2*spacing)
-    return num1 - 10**spacing * num2
-
-
-def mixin_factors(num, add_to_set):
-    factorization = factorint(num)
-    for factor in factorization:
-        mult = factorization[factor]  # appears to always be 1
-        add_to_set.add(factor)
-
-
-factor_set = set()
-for exp in range(6):
-    num = spaced_ones(2, 2**exp)
-    mixin_factors(num, factor_set)
-    print("2, 2**exp")
-
-for i in range(0, 6):
-    for j in range(0, 2):
-        num = alternating_ones(5**j, 2**i)
-        mixin_factors(num, factor_set)
-        print("alternating 5**j, 2**i")
-
-for i in range(0, 4):
-    for j in range(0, 2):
-        num = spaced_ones(5, 2**i * 5**j)
-        mixin_factors(num, factor_set)
-        print("2**i, 5**j")
-
-solution = sorted(list(factor_set))
-print(solution[:40])
-print(sum(solution[:40]))
-
-for pow in range(4, 5):
-    big_ass_number = 0
-    for i in range(10**pow):
-        big_ass_number += 10**i
-
+def get_factors():
     count = 0
-    prime_list = []
-    for p in primerange(1000000):
-        while big_ass_number % p == 0:
-            big_ass_number //= p
-            count += 1
-            prime_list.append(p)
-            if count == 40:
-                break
-        else:
-            continue
-        break
 
-    print(prime_list)
-    print(count, sum(prime_list))
+    for i in range(0, 9):
+        for j in range(0, 9):
+            p = 2**i * 5**j + 1
+
+            if p >= 200_000:
+                break
+            if 5 < p and isprime(p):
+                factor_list.add(p)
+
+    for p in primerange(3, 200_000):
+        selected_prime = False
+        for i in range(0, 9):
+            for j in range(0, 9):
+                s = 2**i * 5**j
+                if s > 10_000:
+                    continue
+
+                repunit = int("1" * s)
+                if repunit % p == 0:
+                    if p not in factor_list:
+                        print(str(p) + " not of form 2**i * 5**j + 1")
+                        factor_list.add(p)
+
+                    count += 1
+                    selected_prime = True
+
+                sigma = 10**s + 1
+                if sigma % p == 0:
+                    factor_list.add(p)
+                    count += 1
+                    selected_prime = True
+
+                if selected_prime:
+                    break
+            if selected_prime:
+                break
+        if count >= 40:
+            break
+
+
+print(timeit(stmt=get_factors, number=1))
+solution = sorted(factor_list)
+print(solution[:40])
+print(len(solution), sum(solution[:40]))
+
+
+pow = 4
+big_ass_number = 0
+for i in range(10**pow):
+    big_ass_number += 10**i
+
+count = 0
+prime_list = []
+for p in primerange(1000000):
+    while big_ass_number % p == 0:
+        big_ass_number //= p
+        count += 1
+        prime_list.append(p)
+        if count == 40:
+            break
+    else:
+        continue
+    break
+
+print(p)
+print(prime_list)
+print(count, sum(prime_list))
