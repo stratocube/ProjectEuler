4843473900ce3beac44aebd72347b63ea4a5afbf
Nicholas Barnes <nickbaxter883@gmail.com>
2022-05-16 21:16:07 -0500

    Problem 133
    Much shorter, faster, elegant solution to p132. Probably the intended solution.

--- src/p126-150/p132.py
+++ src/p126-150/p132.py
@@ -1,75 +1,24 @@
-from sympy import primerange, isprime
-from timeit import timeit
+from sympy import primerange
+from math import gcd
+import timeit
 
 factor_list = set()
 
-def get_factors():
+def get_factors(n):
     count = 0
+    for p in primerange(7, 200_000):
+        g = gcd(n, p-1)
+        if 10**g % p == 1:
+            factor_list.add(p)
+            count += 1
 
-    for i in range(0, 9):
-        for j in range(0, 9):
-            p = 2**i * 5**j + 1
-
-            if p >= 200_000:
-                break
-            if 5 < p and isprime(p):
-                factor_list.add(p)
-
-    for p in primerange(3, 200_000):
-        selected_prime = False
-        for i in range(0, 9):
-            for j in range(0, 9):
-                s = 2**i * 5**j
-                if s > 10_000:
-                    continue
-
-                repunit = int("1" * s)
-                if repunit % p == 0:
-                    if p not in factor_list:
-                        print(str(p) + " not of form 2**i * 5**j + 1")
-                        factor_list.add(p)
-
-                    count += 1
-                    selected_prime = True
-
-                sigma = 10**s + 1
-                if sigma % p == 0:
-                    factor_list.add(p)
-                    count += 1
-                    selected_prime = True
-
-                if selected_prime:
-                    break
-            if selected_prime:
-                break
         if count >= 40:
             break
 
 
-print(timeit(stmt=get_factors, number=1))
+t = timeit.Timer(lambda: get_factors(10**9))
+print(t.timeit(1))
+
 solution = sorted(factor_list)
 print(solution[:40])
 print(len(solution), sum(solution[:40]))
-
-
-pow = 4
-big_ass_number = 0
-for i in range(10**pow):
-    big_ass_number += 10**i
-
-count = 0
-prime_list = []
-for p in primerange(1000000):
-    while big_ass_number % p == 0:
-        big_ass_number //= p
-        count += 1
-        prime_list.append(p)
-        if count == 40:
-            break
-    else:
-        continue
-    break
-
-print(p)
-print(prime_list)
-print(count, sum(prime_list))
---
+++ src/p126-150/p133.py
@@ -0,0 +1,35 @@
+from sympy import primerange
+from math import gcd
+import timeit
+
+factor_list = {2, 3, 5}
+
+
+def get_factors():
+    for p in primerange(7, 100_000):
+        can_be_factor = False
+        x = p-1
+
+        two_count = 0
+        while x % 2 == 0:
+            x //= 2
+            two_count += 1
+
+        five_count = 0
+        while x % 5 == 0:
+            x //= 5
+            five_count += 1
+
+        for i in range(two_count + 1):
+            for j in range(five_count + 1):
+                g = 2**i * 5**j
+                if 10**g % p == 1:
+                    can_be_factor = True
+
+        if not can_be_factor:
+            factor_list.add(p)
+
+
+t = timeit.Timer(lambda: get_factors())
+print(t.timeit(1))
+print(sum(factor_list))
